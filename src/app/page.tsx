5530fb5552c28c6c1db826c0172e0700c4df3ce0
Mark Estareja <mark.estareja.me@gmail.com>
2026-04-14 12:49:16 +0800

    Finished the responsive design in form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,19 @@ export default function Home() {
     router.push(`/prediction/${inputValue}`);
   }
   return (
-    <div className="w-auto flex flex-col gap-6 items-center bg-light-color rounded-xl p-9">
+    <div className="w-auto flex flex-col gap-12 items-center bg-light-color rounded-xl p-9 drop-shadow-lg">
       <div>
         <h1 className="text-dark-color font-black text-2xl md:text-4xl lg:text-6xl whitespace-nowrap">PREDICTION APP</h1>
       </div>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-full">
         <input 
           type="text" 
           placeholder="Type Your Name..."  
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-          className="text-base md:text-xl w-full p-4 italic text-center bg-lighter-color"
+          className="text-base md:text-xl w-full p-4 italic text-center bg-lighter-color rounded"
         />
-        <button type="submit">Predict Data</button>
+        <button type="submit" className="bg-dark-color py-4 px-12 text-lighter-color text-lg md:text-2xl lg:text-3xl rounded w-auto">Predict Data</button>
       </form>
     </div>
   );
